58a58ead021b7c216b4307066f1d50e1e66035a1
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2024-01-13 12:13:27 +0300

    Добавлен удаление продукта и изменение цены. Добавлено добавление и удаление категории.

---
+++ WebTutorial/Controllers/CategoryController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebTutorial.Models;
+
+namespace WebTutorial.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : Controller
+    {
+        [HttpPost("addCategory")]
+        public IActionResult AddCategory([FromQuery] string categoryName, string catgoryDescription)
+        {
+            try
+            {
+                using (var context = new StoreContext())
+                {
+
+                    if (!context.Categories.Any(x => x.Name.ToLower().Equals(categoryName.ToLower())))
+                    {
+                        Category category = new Category() { Name = categoryName, Description = catgoryDescription };
+                        context.Categories.Add(category);
+                        context.SaveChanges();
+                        return Ok(category.Id);
+                    }
+                    else
+                        return StatusCode(409);
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("delCategory")]
+        public IActionResult DeleteCategory([FromQuery] string name)
+        {
+            try
+            {
+                using (var context = new StoreContext())
+                {
+                    var category = context.Categories.FirstOrDefault(x => x.Name.ToLower().Equals(name.ToLower()));
+                    if (category != null)
+                    {
+                        context.Categories.Remove(category);
+                        context.SaveChanges();
+                        return Ok(category.Id);
+                    }
+                    else
+                        return StatusCode(409);
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
--- WebTutorial/Controllers/ProductController.cs
+++ WebTutorial/Controllers/ProductController.cs
@@ -36,27 +36,73 @@ namespace WebTutorial.Controllers
             {
                 using (var context = new StoreContext())
                 {
-                    Category category = null;
-                    if(!context.Categories.Any(x => x.Name.ToLower().Equals(name.ToLower()))) {
+                    Category category = context.Categories.FirstOrDefault(x => x.Name.ToLower().Equals(categoryName.ToLower()));
+                    if (category == null)
+                    {
                         category = new Category() { Name = categoryName };
                         context.Categories.Add(category);
                         context.SaveChanges();
-                    }
-                    else
-                    {
-                        category = context.Categories.FirstOrDefault(x => x.Name.ToLower().Equals(name.ToLower()));
-                    }
+                    }                    
 
                     if (!context.Products.Any(x => x.Name.ToLower().Equals(name.ToLower())) && category != null)
                     {
-                        var product = new Product() { Name = name, Description = description, Category = category, CategoryId = category.Id, Price = price};
+                        var product = new Product() { Name = name, Description = description, Category = category, CategoryId = category.Id, Price = price };
                         context.Add(product);
                         context.SaveChanges();
                         return Ok(product.Id);
                     }
                     else
                         return StatusCode(409);
-                    
+
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("delProduct")]
+        public IActionResult DeleteProduct([FromQuery] string name)
+        {
+            try
+            {
+                using (var context = new StoreContext())
+                {
+                    var product = context.Products.FirstOrDefault(x => x.Name.ToLower().Equals(name.ToLower()));
+                    if (product != null)
+                    {
+                        context.Products.Remove(product);
+                        context.SaveChanges();
+                        return Ok(product.Id);
+                    }
+                    else
+                        return StatusCode(409);
+
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPatch("addProductPrice")]
+        public IActionResult AddProductPrice([FromQuery] string name, int price)
+        {
+            try
+            {
+                using (var context = new StoreContext())
+                {
+                    var product = context.Products.FirstOrDefault(x => x.Name.ToLower().Equals(name.ToLower()));
+                    if (product != null)
+                    {
+                        product.Price = price;
+                        context.SaveChanges();
+                        return Ok(product.Price);
+                    }
+                    else
+                        return StatusCode(409);
                 }
             }
             catch
